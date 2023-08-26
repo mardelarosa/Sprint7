a9836d76374074783acbfa0420e5200fb970efd3
marmoyalopez <124144581+marmoyalopez@users.noreply.github.com>
2023-08-26 21:54:54 +0200

    Eliminar contenido de la rama dev

--- src/pages/Calculator/PanellStyled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const StyledPanell = styled.div`
-  background-color: ${({ theme }) => theme.colors.fonsPagina};
-  padding: ${({ theme }) => theme.spacing.padding || '20px'};
-  margin-top: ${({ theme }) => theme.spacing.margin};
-  color: ${({ theme }) => theme.colors.textButton};
-`;
